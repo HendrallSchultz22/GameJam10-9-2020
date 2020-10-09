0aae438efa491448db01d56b10f22b7f5d7d1830
JohnPinckney <pinckney.john@rocketmail.com>
2020-10-09 19:40:13 -0400

    Start work on skeleton AI

---
+++ TestScene/Test_Game_Jam/Assets/Scripts/SkeletonAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SkeletonAI : MonoBehaviour
+{
+    [Tooltip("In most instances this should be set to the player")]
+    public Transform target;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (target == null) Debug.Log("AI: " + gameObject.name + " does not have a target.");
+    }
+
+    private void Update()
+    {
+        if (target != null) agent.SetDestination(target.position);
+    }
+}
